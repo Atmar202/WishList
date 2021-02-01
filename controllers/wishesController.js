de06a32d8f29b594a519f4e99c0bcd697c2b4e3f
Opilane TTHK <opilane@tthk.ee>
2021-02-01 13:04:22 +0200

    Commit2

--- controllers/wishesController.js
+++ controllers/wishesController.js
@@ -1,3 +1,18 @@
+const Wish = require('../models/wishList');
+
 exports.getWishPage = (req, res)=> {
-    res.render('index.ejs');
-}
+    Wish.fetchWishes(wishList => {
+         res.render('index.ejs', {wishItems: wishList});
+         console.log(wishList);
+    });
+};
+exports.postnewWish = (req, res) =>{
+    let wish = new Wish(req.body.newWish, req.body.newPrice);
+    wish.saveWish();
+    res.redirect('/');
+};
+
+exports.deleteWish = (req, res) =>{
+    Wish.deleteWish(req.body.checkbox);
+    res.redirect('/');
+};
--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,7 @@ const wishesController = require('../controllers/wishesController');
 const router = express.Router();
 
 router.get('/', wishesController.getWishPage);
+router.post('/', wishesController.postnewWish);
+router.post('/delete', wishesController.deleteWish);
 
 module.exports = router;
